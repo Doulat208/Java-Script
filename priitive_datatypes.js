e87253beb1f0daddc20f075325956f76f360664d
Doulat Biswal <doulatbiswal209@gmail.com>
2025-08-01 16:55:07 +0530

    Primitive value Type

---
+++ priitive_datatypes.js
@@ -0,0 +1,35 @@
+// Primitive / value type
+// String, Number, Boolean, Undefined, NULL, Symbol
+// Js - Dynamic Language ( here we can change the value type / data type of the variables)
+
+// String
+let  x = "hello"
+console.log(x);
+console.log(typeof(x));
+
+// change the type of x from String to Number
+x = 5
+console.log(x)
+console.log(typeof(x));
+
+// Number
+let  y  = 15
+console.log(y)
+console.log(typeof(y))
+
+let z = 17.89
+console.log(z)
+console.log(typeof(z))
+
+// Boolean
+let t = true
+console.log(t)
+console.log(typeof(t))
+
+// default vaalue - undefined
+let w;
+console.log(w)
+console.log(typeof(w))
+
+w = "hyy"
+console.log(w)
--- test.js
+++
@@ -1,3 +0,0 @@
-let  paaragraph 
-console.log(paaragraph);
-
